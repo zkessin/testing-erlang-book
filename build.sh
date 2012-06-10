8fbb8850b904ce39dfc35bc4a965d3b91f492d51
Zachary Kessin <zkessin@gmail.com>
2012-06-10 16:50:09 +0300

    changed from .txt to .doc extentions

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for chapter in `ls *.txt` 
+for chapter in `ls *.doc` 
 do 
     asciidoc --backend=html4 --out-file=html/$chapter.html $chapter
 done
+cp html/book.doc.html html/index.html
