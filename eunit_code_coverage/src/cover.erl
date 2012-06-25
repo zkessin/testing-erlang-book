583092ffe9ce9a6c95ecc2d9dba93c5ade9de44f
Zachary Kessin <zkessin@gmail.com>
2012-06-25 08:57:57 +0300

    example module

---
+++ eunit_code_coverage/src/cover.erl
@@ -0,0 +1,29 @@
+-module(cover).
+-export([ cover/0]).
+
+cover()->
+    io:format("~n********************************************************************************~n"),
+    io:format("Running Tests~n~n"),    
+
+    cover:start(),
+    _R			= cover:compile_beam_directory("ebin/"),
+    
+    Modules		= cover:modules(),
+  
+    [eunit:test(Module) || Module <- Modules],
+    
+    Coverage		= [cover:analyse(Module, calls, line) || Module <- Modules],
+    L			= lists:merge([Lines || {ok, Lines} <- Coverage]),
+    NonTestModules      = lists:filter(fun({{Module, _}, _} )->
+						  Es = lists:reverse(string:tokens(atom_to_list(Module),"_") ),
+						  lists:nth(1, Es)  =/= "test"
+					  end,L),
+    Zeros		= lists:filter(fun ({{_Module, _Line}, Count}) ->					       
+					       Count	=:= 0
+				       end, NonTestModules),
+    [io:format("src/~p.erl:~p: No Code Coverage~n",[Module, Line])|| {{Module, Line}, 0} <-Zeros],
+    init:stop().
+    
+    
+    
+
