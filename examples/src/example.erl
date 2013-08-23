a2c3aa4e44d2d793741de351f6f34b8f5f936127
Zachary Kessin <zkessin@gmail.com>
2013-08-23 08:58:27 +0300

    Improvements to eunit chapter

--- examples/src/example.erl
+++ examples/src/example.erl
@@ -1,4 +1,4 @@
--module(never_match).
+-module(example).
 -compile(export_all).
 -type component_id() ::binary().
 
@@ -15,4 +15,3 @@ get_json_from_guid(Guid) ->
 	{error,not_found} ->
 	    {error, not_found}
     end.
-x
